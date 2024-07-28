ffca84a56e96a55f6f562fb90e5625a4bf0067fb
richard.frolkovic@event-horizon.sk <richard.frolkovic@event-horizon.sk>
2024-07-28 19:05:50 +0200

    Updated packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import { defineConfig } from "astro/config";
-import {astroImageTools}  from "astro-imagetools";
-
-
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import {astroImageTools}  from "astro-imagetools"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
