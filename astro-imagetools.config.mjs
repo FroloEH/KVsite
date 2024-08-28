051d17ee802288b44150b083d00b8da8378b83a6
richard.frolkovic@event-horizon.sk <richard.frolkovic@event-horizon.sk>
2024-08-28 21:07:44 +0200

    Replaced astro image tool with astro image

--- astro-imagetools.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "astro-imagetools/config";
-
-export default defineConfig({
-    placeholder: "blurred",
-    loading: "lazy",
-    breakpoints: [640, 1024, 1280], //in tailwind sm, lg, xl
-  });
