f95fbe1a7e4ba1cc78a32116eccf65a90668de0f
FroloEH <62135975+FroloEH@users.noreply.github.com>
2024-09-13 13:52:11 +0200

    Ran prettier everywhere (#16)

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,31 +1,35 @@
-import { defineAction } from 'astro:actions';
-import { z } from 'astro:schema';
+import { defineAction } from 'astro:actions'
+import { z } from 'astro:schema'
 
 export const server = {
   getAccessToken: defineAction({
     input: z.string(),
     handler: async (code) => {
       const params = {
-        "client_id" : import.meta.env.CLIENT_ID,
-        "client_secret" : import.meta.env.CLIENT_SECRET,
-        "code" : code
+        client_id: import.meta.env.CLIENT_ID,
+        client_secret: import.meta.env.CLIENT_SECRET,
+        code: code,
       }
-      const result = await fetch("https://github.com/login/oauth/access_token", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+      const result = await fetch(
+        'https://github.com/login/oauth/access_token',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(params),
         },
-        body: JSON.stringify(params),
-      });
-      if(result.status === 200){
-        const formData = await result.formData();
-        const token = formData.get('access_token')?.toString();
-        console.log(`authorization:github:"success":${JSON.stringify({ token })}`);
-        return token;
+      )
+      if (result.status === 200) {
+        const formData = await result.formData()
+        const token = formData.get('access_token')?.toString()
+        console.log(
+          `authorization:github:"success":${JSON.stringify({ token })}`,
+        )
+        return token
+      } else {
+        console.error(result)
       }
-      else {
-        console.error(result);
-      }
-    }
-   })
+    },
+  }),
 }
