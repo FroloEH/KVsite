729c6a5c9f3c5b1abb93737e9d84a695b471decc
FroloEH <62135975+FroloEH@users.noreply.github.com>
2024-09-14 13:31:28 +0200

    Added adapter and removed old images (#42)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly CLIENT_SECRET: string;
+    readonly CLIENT_ID: string;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
