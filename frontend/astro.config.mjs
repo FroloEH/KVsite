e2c4c2445d1aeca8e45cafd8b38f8b895d1b137e
richard.frolkovic@event-horizon.sk <richard.frolkovic@event-horizon.sk>
2024-09-20 18:55:09 +0200

    Fixed issue with admin config Finished mapping footer to cms

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-});
+  redirects: {
+    '/config.yml': '/admin/config.yml',
+  },
+})
