56fa73c00d8a73f902d123c9e619b1997a9cc07a
FroloEH <62135975+FroloEH@users.noreply.github.com>
2024-11-08 18:55:05 +0100

    About us page (#87)
    * Removed root level pakcakage json.
    
    * Added header menu logic
    
    * Conneeccted header menu to cms
    
    * Finished up a static about us page
    
    * Fixed coderabbit config
    
    * Included content in coderabbit review
    Added semantic html to address for accessibility
    
    * Removing logging, set coderabbit to picky setting
    
    * Update frontend/src/components/Header.astro
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    Signed-off-by: FroloEH <62135975+FroloEH@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: FroloEH <62135975+FroloEH@users.noreply.github.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- frontend/src/types/pages.ts
+++ frontend/src/types/pages.ts
@@ -15,3 +15,17 @@ export interface FooterContent {
   footer_cta_link: string
   footer_cta_button_text: string
 }
+
+export interface HeaderLink {
+  link_text: string
+  link_target: string
+}
+
+export interface HeaderContent {
+  header_1st_column_links: HeaderLink[]
+  header_2nd_column_links: HeaderLink[]
+  header_3rd_column_links: HeaderLink[]
+  header_1st_column_header: string
+  header_2nd_column_header: string
+  header_3rd_column_header: string
+}
