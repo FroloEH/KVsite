c4d793cba891fb7af5964e211ef07d9ebae2923b
richard.frolkovic@event-horizon.sk <richard.frolkovic@event-horizon.sk>
2025-03-15 12:38:14 +0100

    Add Umami analytics configuration and update Header component

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,10 +1,5 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-    readonly CLIENT_SECRET: string;
-    readonly CLIENT_ID: string;
-  }
   
   interface ImportMeta {
     readonly env: ImportMetaEnv;
