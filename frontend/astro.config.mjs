325deaa6ac7f1907744fa7c062d374915573311d
richard.frolkovic@event-horizon.sk <richard.frolkovic@event-horizon.sk>
2025-06-21 15:10:29 +0200

    Add pagefind integration and search component; update layout and build scripts

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import pagefind from 'astro-pagefind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), pagefind()],
   output: 'static',
 })
