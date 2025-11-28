9b485c01dec830d8a93e742d5c916e3ff35036f8
TheMartes <hello@themartes.com>
2025-11-28 20:23:18 +0100

    feat: add sanity integration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,19 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import pagefind from 'astro-pagefind'
+import sanity from '@sanity/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), pagefind()],
+  integrations: [
+    tailwind(),
+    pagefind(),
+    sanity({
+        projectId: 'sb0cunx0',
+        dataset: 'production',
+        useCdn: false, // for static builds
+        studioBassePath: '/admin' 
+    }),
+  ],
   output: 'static',
 })
