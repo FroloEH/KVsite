b6f395034b885d45dad9d9dfa1805ccbc4b713b4
FroloEH <62135975+FroloEH@users.noreply.github.com>
2026-04-03 12:51:39 +0200

    Added new webhook for group form (#304)

--- backend/src/pages/api/tally-character.ts
+++ backend/src/pages/api/tally-character.ts
@@ -3,16 +3,13 @@ import { processTallyWebhook } from '../../lib/webhook'
 
 export const POST = (async ({ request }) => {
   try {
-    // Decode the request body as Latin-1 to handle special characters properly
-    // const buffer = await request.arrayBuffer()
-    // const text = new TextDecoder('latin1').decode(buffer)
     const payload = await request.json()
     const result = await processTallyWebhook(payload, {
       SHAREPOINT_TENANT: import.meta.env.SHAREPOINT_TENANT,
       SHAREPOINT_CLIENT_ID: import.meta.env.SHAREPOINT_CLIENT_ID,
       SHAREPOINT_CLIENT_SECRET: import.meta.env.SHAREPOINT_CLIENT_SECRET,
       SHAREPOINT_SITE_ID: import.meta.env.SHAREPOINT_SITE_ID,
-      SHAREPOINT_LIST_ID: import.meta.env.SHAREPOINT_LIST_ID
+      SHAREPOINT_LIST_ID: import.meta.env.SHAREPOINT_CHARACTER_LIST_ID
     })
 
     if (result.success) {
